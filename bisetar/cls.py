7d6df511990d18f4435b2afeeaaaf13b0c6406c9
wilson-ye-chen <ye.chen@sydney.edu.au>
2024-07-01 15:16:34 +1000

    ADD: conditional least squares

---
+++ bisetar/cls.py
@@ -0,0 +1,29 @@
+import numpy as np
+from bisetar.mcmc import BiDirSetarRwm
+
+class BiDirSetarCls(BiDirSetarRwm):
+    def __init__(self, x):
+        super().__init__(x)
+        p = np.arange(0.1, 0.9, 0.01)
+        q = np.quantile(x, p)
+        r1, r2 = np.meshgrid(q, q)
+        r1 = r1.reshape(-1, 1)
+        r2 = r2.reshape(-1, 1)
+        self.r_grid = np.hstack(r1, r2)
+
+    def cls(self, r_grid=None):
+        if r_grid == None:
+            r_grid = self.r_grid
+
+        n_grid = r_grid.shape[0]
+        v_ttl = np.zeros(n_grid)
+        for i in range(n_grid):
+            xr = self.splitx(r_grid[i])
+            for j in range(4):
+                if len(xr[j][0]) < 5:
+                    v_ttl[i] = np.nan
+                else:
+                    v = self.lsfit(xr[j][0], xr[j][1], xr[j][2])[1]
+                    v_ttl[i] += v
+        i_min = np.nanargmin(v_ttl)
+        return(r_grid[i_min], v_ttl[i_min], v_ttl)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bisetar',
-    version='0.2.0',
+    version='0.3.0',
     description='MCMC for Bayesian Bidirectional SETAR model',
     url='https://github.com/wilson-ye-chen/bisetar',
     author='Wilson Ye Chen',
