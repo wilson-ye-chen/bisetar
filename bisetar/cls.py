fa0d7c8de061c48520d45a8e96f3771acf2e7532
wilson-ye-chen <ye.chen@sydney.edu.au>
2024-08-31 09:59:03 +1000

    ADD: method for estimating regime specific parameters

--- bisetar/cls.py
+++ bisetar/cls.py
@@ -11,7 +11,7 @@ class BiSetarCls(BiSetar):
         r2 = r2.reshape(-1, 1)
         self.r_grid = np.hstack((r1, r2))
 
-    def cls(self, r_grid=None):
+    def learn_r(self, r_grid=None):
         if r_grid == None:
             r_grid = self.r_grid
 
@@ -27,6 +27,15 @@ class BiSetarCls(BiSetar):
                     v_ttl[i] += v
         i_min = np.nanargmin(v_ttl)
         return(r_grid[i_min], v_ttl[i_min], v_ttl)
+
+    def learn_phi(self, r):
+        xr = self.splitx(r)
+        phi = np.empty((4, 4))
+        for i in range(4):
+            b, v = self.lsfit(xr[i][0], xr[i][1], xr[i][2])[:2]
+            phi[i, :3] = b
+            phi[i, 3] = v
+        return phi.flatten()
 
     def find_feasible(self, n_min):
         n_grid = self.r_grid.shape[0]
