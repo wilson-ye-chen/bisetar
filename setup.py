6ef37d59d78f90f7fbcfc85aad2438612e1cb36d
wilson-ye-chen <ye.chen@sydney.edu.au>
2025-03-03 15:48:47 +1100

    FIX: version count

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bisetar',
-    version='0.9.11',
+    version='0.9.12',
     description='MCMC for Bayesian Bidirectional SETAR model',
     url='https://github.com/wilson-ye-chen/bisetar',
     author='Wilson Ye Chen',
